98e346cd8c4f00a59c8fcbf68523332600dcbb67
Genthh <gentihulaj12@gmail.com>
2024-05-29 12:40:16 +0200

    finished footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,75 +1,77 @@
 import React from "react";
-
+import Logo from "../assets/logo.svg";
 export default function Footer() {
   return (
     <div
-      className="flex flex-col bg-[#DDC7BB] h-[50vh] px-5 mx-1"
+      className="flex flex-col bg-[#DDC7BB] h-[50vh] px-10 mx-1"
       style={{ borderTopRightRadius: "10px", borderTopLeftRadius: "10px" }}
     >
-      <div className="flex justify-between py-10">
+      <div className="flex justify-between py-10 pt-32">
         <div>
-          <h2 className="text-2xl font-bold">Dwelle</h2>
-          <p>Bringing you closer to your dream home, one click at a time.</p>
+          <Logo />
+          <p className="text-xl pt-5 w-1/2 ">
+            Bringing you closer to your dream home, one click at a time.
+          </p>
         </div>
-        <div className="flex space-x-16">
+        <div className="flex gap-[120px]">
           <div>
-            <h3 className="font-bold">About</h3>
-            <ul>
-              <li>
+            <h3 className="text-3xl font-bold">About</h3>
+            <ul className="text-xl m">
+              <li className="mt-2">
                 <a href="#">Our Story</a>
               </li>
-              <li>
+              <li className="mt-2">
                 <a href="#">Careers</a>
               </li>
-              <li>
+              <li className="mt-2">
                 <a href="#">Our Team</a>
               </li>
-              <li>
+              <li className="mt-2">
                 <a href="#">Resources</a>
               </li>
             </ul>
           </div>
           <div>
-            <h3 className="font-bold">Support</h3>
-            <ul>
-              <li>
+            <h3 className="text-3xl font-bold">Support</h3>
+            <ul className="text-xl">
+              <li className="mt-2">
                 <a href="#">FAQ</a>
               </li>
-              <li>
+              <li className="mt-2">
                 <a href="#">Contact Us</a>
               </li>
-              <li>
+              <li className="mt-2">
                 <a href="#">Help Center</a>
               </li>
-              <li>
+              <li className="mt-2">
                 <a href="#">Terms of Service</a>
               </li>
             </ul>
           </div>
           <div>
-            <h3 className="font-bold">Find Us</h3>
-            <ul>
-              <li>
+            <h3 className="text-3xl font-bold">Find Us</h3>
+            <ul className="text-xl">
+              <li className="mt-2">
                 <a href="#">Events</a>
               </li>
-              <li>
+              <li className="mt-2">
                 <a href="#">Locations</a>
               </li>
-              <li>
+              <li className="mt-2">
                 <a href="#">Newsletter</a>
               </li>
             </ul>
           </div>
           <div>
-            <h3 className="font-bold">Our Social</h3>
-            <ul>
-              <li>
+            <h3 className="text-3xl font-bold">Our Social</h3>
+            <ul className="text-xl">
+              <li className="mt-2">
                 <a href="#">Instagram</a>
               </li>
-              <li>
+              <li className="mt-2">
                 <a href="#">Facebook</a>
               </li>
-              <li>
+              <li className="mt-2">
                 <a href="#">Twitter (x)</a>
               </li>
             </ul>
